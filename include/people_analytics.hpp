b314e5d982e71d89c670bd400ea3da798687dd84
yckkel1 <sammwang21@gmail.com>
2025-06-21 18:16:21 -0400

    minor changes to not use reference on primitive types

--- include/people_analytics.hpp
+++ include/people_analytics.hpp
@@ -3,12 +3,15 @@
 #include "people.hpp"
 #include <memory>
 #include <vector>
+#include <map>
 
 class PeopleAnalytics {
 private:
     std::vector<std::unique_ptr<People>> people_arr;
+    void get_age_grouped_names_helper(std::map<std::string, std::vector<std::string>>& group_map, const std::string& group_name, int age_lower_bound, int age_higher_bound);
 public:
     PeopleAnalytics() = default;
     void add(const std::string& name, int age, double height); // Add this to people_arr internally
     std::vector<std::string> get_names_of_tall_adults(double min_height); // return names of all people taller than min_height
+    std::map<std::string, std::vector<std::string>> get_age_grouped_names(); // map people to different age groups 
 };
--- src/people_analytics.cpp
+++ src/people_analytics.cpp
@@ -14,3 +14,25 @@ std::vector<std::string> PeopleAnalytics::get_names_of_tall_adults(double min_he
     });
     return std::vector<std::string>(name_view.begin(), name_view.end());
 }
+
+std::map<std::string, std::vector<std::string>> PeopleAnalytics::get_age_grouped_names() {
+    // "Child" -> age < 18
+    // "Adult" -> 18-64
+    // "Senior" -> 65+
+    std::map<std::string, std::vector<std::string>> group_map;
+    get_age_grouped_names_helper(group_map, "Child", 0, 17);
+    get_age_grouped_names_helper(group_map, "Adult", 18, 64);
+    get_age_grouped_names_helper(group_map, "Senior", 65, 200); // arbitrary big enough age as cap
+    return group_map;
+}
+
+void PeopleAnalytics::get_age_grouped_names_helper(std::map<std::string, std::vector<std::string>>& group_map, const std::string& group_name, int age_lower_bound, int age_higher_bound) {
+    auto view_filtered_by_age = people_arr | std::views::filter([age_lower_bound, age_higher_bound](const std::unique_ptr<People>& people) {
+        return people->get_age() >= age_lower_bound && people->get_age() <= age_higher_bound;
+    });
+    auto names_filtered_by_age = view_filtered_by_age | std::views::transform([](const std::unique_ptr<People>& people) {
+        return people->get_name();
+    });
+    group_map[group_name] = std::vector<std::string> (names_filtered_by_age.begin(), names_filtered_by_age.end());
+    std::ranges::sort(group_map[group_name]);
+}
